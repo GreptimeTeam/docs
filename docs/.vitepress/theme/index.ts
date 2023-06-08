97ca301dd514df8782fb57934c8dff13f882e98d
nemo <122499469@qq.com>
2023-06-08 11:07:12 +0800

    fix: fix ga

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,22 +3,20 @@ import DefaultTheme from 'vitepress/theme'
 import Layout from 'vitepress/dist/client/theme-default/Layout.vue'
 import './style/index.styl'
 
-function initGA() {
-  const oScript = document.createElement('script')
-
-  oScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-BYNN8J57JZ'
-  document.body.appendChild(oScript)
-
-  window.dataLayer = window.dataLayer || []
-  gtag('js', new Date())
-  gtag('config', 'G-BYNN8J57JZ')
-}
-
-initGA()
-
 export default {
   ...DefaultTheme,
   Layout,
   NotFound: () => '',
-  enhanceApp({ app }) {},
+  enhanceApp(app) {
+    if (typeof window !== 'undefined') {
+      const oScript = window.document.createElement('script')
+
+      oScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-BYNN8J57JZ'
+      window.document.body.appendChild(oScript)
+
+      window.dataLayer = window.dataLayer || []
+      gtag('js', new Date())
+      gtag('config', 'G-BYNN8J57JZ')
+    }
+  },
 }
