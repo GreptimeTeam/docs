59f56e069fc000101ca10ef147aa6f423606ba48
nemo <122499469@qq.com>
2023-06-16 14:38:37 +0800

    fix: refactor config

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -2,36 +2,16 @@ import { enConfig } from './en'
 import { zhConfig } from '../../zh/config/index'
 import { common } from './common'
 import dotenv from 'dotenv'
+import merge from 'deepmerge'
 
 export default async () => {
   const commonConfig = await common()
 
-  const zh = { label: '简体中文', lang: 'zh', ...(await zhConfig()) }
-  const en = { label: 'English', lang: 'en', ...(await enConfig()) }
-  let root = en
-  let rewrites = {}
-
-  switch (dotenv.config().parsed?.VITE_LANG) {
-    case 'zh':
-      root = zh
-      rewrites = {
-        'zh/:file': ':file',
-        'zh/:group/:file': ':group/:file',
-        'zh/:group/:type/:file': ':group/:type/:file',
-      }
-      break
-    default:
-      root = en
-      rewrites = {}
+  const lang = {
+    zh: await zhConfig(),
+    en: await enConfig(),
   }
 
-  return {
-    ...commonConfig,
-    locales: {
-      root,
-      en: { ...en, link: 'https://docs.greptime.com/' },
-      zh: { ...zh, link: 'https://docs.greptime.cn/' },
-    },
-    rewrites,
-  }
+  const localConfig = lang[dotenv.config().parsed?.VITE_LANG]
+  return merge(commonConfig, localConfig)
 }
