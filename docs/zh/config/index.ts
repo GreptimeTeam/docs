08cf1ee6056da7cc7d22d7279cc33ca643df39a8
Agan <104718452+Aganivi@users.noreply.github.com>
2023-09-15 18:05:31 +0800

    feat: update search (#570)

--- docs/zh/config/index.ts
+++ docs/zh/config/index.ts
@@ -28,9 +28,32 @@ export const zhConfig = async () => ({
     'zh/:group/:type/:file/:name': ':group/:type/:file/:name',
   },
   locales: {
-    root: { label: '简体中文' },
+    root: { label: '简体中文', lang: 'zh-CN' },
   },
   themeConfig: {
+    search: {
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档',
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
     nav: [
       {
         text: '主页',
