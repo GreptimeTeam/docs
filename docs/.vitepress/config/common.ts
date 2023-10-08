5b7bfd2fe7629de0619b3d1fb501ca6cf1480c62
Agan <104718452+Aganivi@users.noreply.github.com>
2023-10-08 15:15:12 +0800

    feat: simplified rewrite and update version of vitepress (#592)

--- docs/.vitepress/config/common.ts
+++ docs/.vitepress/config/common.ts
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv'
+
+export const CURRENT_LANG = dotenv.config().parsed?.VITE_LANG || 'en'
 export const CURRENT_VERSION = 'v0.4'
-export const currentVersionPath = `/${CURRENT_VERSION}/`
+
+const currentVersionPath = `${CURRENT_LANG}/${CURRENT_VERSION}/:path+`
+const versionPath = `${CURRENT_LANG}/:version/:path+`
 
 export const common = async () => {
   return {
@@ -10,12 +15,19 @@ export const common = async () => {
       zh: { label: '简体中文', lang: 'zh-CN', link: 'https://docs.greptime.cn/' },
       en: { label: 'English', lang: 'en-US', link: 'https://docs.greptime.com/' },
     },
+    markdown: {
+      theme: { light: 'material-theme-darker', dark: 'material-theme-darker' },
+    },
+    rewrites: {
+      [currentVersionPath]: `:path+`,
+      [versionPath]: `:version(v\\d\.\\d)?/:path+`,
+    },
     themeConfig: {
       search: {
         provider: 'local',
       },
       siteTitle: '',
-      logo: 'logo-text-tinted.png',
+      logo: '/logo-text-tinted.png',
       copyright: '©Copyright 2022 Greptime Inc. All Rights Reserved',
       email: 'marketing@greptime.com',
       editLink: {
