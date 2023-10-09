d533bbe01454ffd828721a955179d8f4c2f21e4f
Agan <xionggan@greptime.com>
2023-10-09 17:15:40 +0800

    perf(sidebar): optimize function of insertIcon

--- docs/.vitepress/theme/utils.ts
+++ docs/.vitepress/theme/utils.ts
@@ -9,23 +9,29 @@ export const gtag = function () {
 //NOTICE: use /i to ignore case
 export const isBlog = url => /blogs\//i.test(url)
 
-export const insertIcon = (iconMap, event = false) => {
-  const div = document.getElementById('VPSidebarNav')
-  if (div) {
-    const child = div.children
-    const group = Array.from(child).filter(value => value.className === 'group')
-    group.forEach(item => {
-      const parentElement = item.children[0].children[0]
-      const targetElement = <HTMLInputElement>parentElement.children[1]
-      if (targetElement.name) return
-      const insertElement = <HTMLInputElement>document.createElement('iconpark-icon')
-      const key = parentElement.textContent
-      insertElement.name = iconMap.find(value => value.key === key)?.icon || ''
-      parentElement.insertBefore(insertElement, targetElement)
-    })
-    return
-  }
-  event && setTimeout(() => insertIcon(iconMap, event), 50)
+export const insertIcon = iconMap => {
+  const domObserver = new MutationObserver((_mutationList, observer) => {
+    const div = document.getElementById('VPSidebarNav')
+
+    if (div) {
+      const child = div.children
+      const group = Array.from(child).filter(value => value.className === 'group')
+      group.forEach(item => {
+        const parentElement = item.children[0].children[0]
+        const targetElement = <HTMLInputElement>parentElement.children[1]
+        if (targetElement.name) {
+          observer.disconnect()
+          return
+        }
+        const insertElement = <HTMLInputElement>document.createElement('iconpark-icon')
+        const key = parentElement.textContent
+        insertElement.name = iconMap.find(value => value.key === key)?.icon || ''
+        parentElement.insertBefore(insertElement, targetElement)
+      })
+      observer.disconnect()
+    }
+  })
+  domObserver.observe(document.body, { childList: true, subtree: true })
 }
 
 export function getSidebarIcon(iconMap) {
@@ -33,7 +39,7 @@ export function getSidebarIcon(iconMap) {
   window.addEventListener('click', e => {
     const el = <HTMLInputElement>e.target
     if (el.matches('.VPLink')) {
-      insertIcon(iconMap, true)
+      insertIcon(iconMap)
     }
   })
 }
