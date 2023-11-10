9ca7dd96ce843ae3d537479bcc8825c98372e49d
Agan <xionggan@greptime.com>
2023-11-10 17:15:07 +0800

    feat(script/dev.js): add stdio for display Local while 'npm run dev'

--- script/dev.js
+++ script/dev.js
@@ -4,12 +4,17 @@ const { execSync } = require('node:child_process')
 ;(async () => {
   const files = fs.readdirSync('./docs')
   const versionMap = files.filter(file => file.match(/v\d\.\d/))
-  await execSync(`npm run docs:dev`, {
-    env: {
-      ...process.env,
-      VERSION_MAP: JSON.stringify(versionMap),
-      BASE: '/',
-      ENV: 'dev',
-    },
-  })
+  try {
+    await execSync(`npm run docs:dev`, {
+      stdio: 'inherit',
+      env: {
+        ...process.env,
+        VERSION_MAP: JSON.stringify(versionMap),
+        BASE: '/',
+        ENV: 'dev',
+      },
+    })
+  } catch (error) {
+    if (error.status !== 1) console.log(error)
+  }
 })()
