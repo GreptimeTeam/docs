682470883e0ba1624750311c66ee1d75df60d3ac
Agan <xionggan@greptime.com>
2023-11-10 16:29:10 +0800

    feat(config): adjust some envriment to constant

--- script/build.js
+++ script/build.js
@@ -1,8 +1,5 @@
 const fs = require('fs')
 const { execSync } = require('node:child_process')
-const settingData = require('./setting.json')
-
-const { LATEST_VERSION, websiteMap } = settingData
 
 ;(async () => {
   const files = fs.readdirSync('./docs')
@@ -11,10 +8,8 @@ const { LATEST_VERSION, websiteMap } = settingData
     await execSync(`npm run docs:build`, {
       env: {
         ...process.env,
-        LATEST_VERSION,
         VERSION: versionMap[i],
         VERSION_MAP: JSON.stringify(versionMap),
-        WEBSITE_MAP: JSON.stringify(websiteMap),
         BASE: versionMap[i] && `/${versionMap[i]}/`,
         ENV: 'prod',
       },
--- script/dev.js
+++ script/dev.js
@@ -1,8 +1,5 @@
 const fs = require('fs')
 const { execSync } = require('node:child_process')
-const settingData = require('./setting.json')
-
-const { LATEST_VERSION, websiteMap } = settingData
 
 ;(async () => {
   const files = fs.readdirSync('./docs')
@@ -10,10 +7,7 @@ const { LATEST_VERSION, websiteMap } = settingData
   await execSync(`npm run docs:dev`, {
     env: {
       ...process.env,
-      LATEST_VERSION,
-      VERSION: LATEST_VERSION,
       VERSION_MAP: JSON.stringify(versionMap),
-      WEBSITE_MAP: JSON.stringify(websiteMap),
       BASE: '/',
       ENV: 'dev',
     },
