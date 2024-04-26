9878050c673d3ec74e2f1d0178ef1423de605915
zyy17 <zyylsxm@gmail.com>
2024-04-26 15:51:31 +0800

    refactor: accept additional args for running `pnpm run docs:build`

--- script/build.js
+++ script/build.js
@@ -4,7 +4,13 @@ import { execSync } from 'node:child_process'
   const files = fs.readdirSync('./docs')
   const versionMap = files.filter(file => file.match(/v\d\.\d/))
   for (let i = versionMap.length; i >= 0; i--) {
-    await execSync(`pnpm run docs:build`, {
+    // Retrieve command-line arguments, excluding the first two default arguments (Node.js path and script path).
+    const additionalArgs = process.argv.slice(2)
+    // Concatenate the command with additional arguments.
+    const command = `pnpm run docs:build ${additionalArgs.join(' ')}`;
+    console.log(`Running command: ${command}`)
+
+    await execSync(command, {
       env: {
         ...process.env,
         VERSION: versionMap[i],
