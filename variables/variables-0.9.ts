b77b6275413453eeb269b4a6083cbffcc7148e99
Yiran <cuiyiran3@gmail.com>
2024-08-19 22:44:17 +0800

    Update variables-0.9.ts

--- variables/variables-0.9.ts
+++ variables/variables-0.9.ts
@@ -1,10 +1,8 @@
 export const variables = {
-  grafanaVersion: "9.5.15",
-  greptimedbVersion: "v0.9.1",
-  vectorVersion: "0.33.0-debian",
-  prometheusVersion: "v2.52.0",
-  nodeExporterVersion: "v1.8.0",
-  goSdkVersion: "v0.5.0",
-  javaSdkVersion: "0.7.3"
+  greptimedbVersion: 'v0.9.2',
+  prometheusVersion: 'v2.52.0',
+  nodeExporterVersion: 'v1.8.0',
+  goSdkVersion: 'v0.5.0',
+  javaSdkVersion: '0.7.3',
 };
 
