d6666128e2ba445f46f3b681c18520a826deb3b6
Yiran <cuiyiran3@gmail.com>
2024-12-25 16:36:37 +0800

    update SDK version

--- variables/variables-0.11.ts
+++ variables/variables-0.11.ts
@@ -2,6 +2,6 @@ export const variables = {
   greptimedbVersion: 'v0.11.1',
   prometheusVersion: 'v2.52.0',
   nodeExporterVersion: 'v1.8.0',
-  goSdkVersion: 'v0.5.4',
-  javaSdkVersion: '0.9.1',
+  goSdkVersion: 'v0.6.0',
+  javaSdkVersion: '0.11.0',
 };
