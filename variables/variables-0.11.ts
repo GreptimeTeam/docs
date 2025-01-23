211d0276893b9a74022055d3c0fecbf82987bb7f
sunng87 <sunng87@users.noreply.github.com>
2025-01-23 15:19:20 +0000

    version docs to 0.11.3

--- variables/variables-0.11.ts
+++ variables/variables-0.11.ts
@@ -1,5 +1,5 @@
 export const variables = {
-  greptimedbVersion: 'v0.11.2',
+  greptimedbVersion: 'v0.11.3',
   prometheusVersion: 'v2.52.0',
   nodeExporterVersion: 'v1.8.0',
   goSdkVersion: 'v0.6.0',
