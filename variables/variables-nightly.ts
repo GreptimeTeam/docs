645011130033118cafe9077d1f51a2e8004b2c38
sunng87 <sunng87@users.noreply.github.com>
2025-03-03 00:32:30 +0000

    version docs to 0.13.0-nightly-20250303

--- variables/variables-nightly.ts
+++ variables/variables-nightly.ts
@@ -1,5 +1,5 @@
 export const variables = {
-  greptimedbVersion: 'v0.12.0',
+  greptimedbVersion: 'v0.13.0-nightly-20250303',
   prometheusVersion: 'v2.52.0',
   nodeExporterVersion: 'v1.8.0',
   goSdkVersion: 'v0.6.0',
