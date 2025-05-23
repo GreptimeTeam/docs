1e1bd3ec46dec1010a99e11c5e9c139fc990a41b
daviderli614 <daviderli614@users.noreply.github.com>
2025-05-23 12:34:28 +0000

    version docs to 0.14.3

--- variables/variables-0.14.ts
+++ variables/variables-0.14.ts
@@ -1,5 +1,5 @@
 export const variables = {
-  greptimedbVersion: 'v0.14.2',
+  greptimedbVersion: 'v0.14.3',
   prometheusVersion: 'v2.52.0',
   nodeExporterVersion: 'v1.8.0',
   goSdkVersion: 'v0.6.1',
