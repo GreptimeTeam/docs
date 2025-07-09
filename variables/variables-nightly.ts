bca4d9eb8cd717c1a7acd7c2e5a314132058d7ea
jeremyhi <fengjiachun@gmail.com>
2025-07-09 11:25:32 +0800

    chore: update ingester version

--- variables/variables-nightly.ts
+++ variables/variables-nightly.ts
@@ -2,7 +2,7 @@ export const variables = {
   greptimedbVersion: 'v0.16.0-nightly-20250707',
   prometheusVersion: 'v2.52.0',
   nodeExporterVersion: 'v1.8.0',
-  goSdkVersion: 'v0.6.1',
-  javaSdkVersion: '0.11.0',
+  goSdkVersion: 'v0.6.2',
+  javaSdkVersion: '0.14.6',
   debugPodVersion: '20250421-94c4b8d',
 };
