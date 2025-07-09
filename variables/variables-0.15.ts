4ae8dbce6f64c5c541c7b1446f447d30fa6ac756
jeremyhi <jiachun_feng@proton.me>
2025-07-09 11:45:01 +0800

    chore: update ingester version (#1926)

--- variables/variables-0.15.ts
+++ variables/variables-0.15.ts
@@ -2,7 +2,7 @@ export const variables = {
   greptimedbVersion: 'v0.15.1',
   prometheusVersion: 'v2.52.0',
   nodeExporterVersion: 'v1.8.0',
-  goSdkVersion: 'v0.6.1',
-  javaSdkVersion: '0.11.0',
+  goSdkVersion: 'v0.6.2',
+  javaSdkVersion: '0.14.6',
   debugPodVersion: '20250421-94c4b8d',
 };
