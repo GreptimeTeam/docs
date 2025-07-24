49c3358762238a412812e3c2e76ed733fc1c8a7e
Dennis Zhuang <killme2008@gmail.com>
2025-07-24 17:45:24 +0800

    chore: bump greptimedb version to v0.15.3

--- variables/variables-0.15.ts
+++ variables/variables-0.15.ts
@@ -1,5 +1,5 @@
 export const variables = {
-  greptimedbVersion: 'v0.15.2',
+  greptimedbVersion: 'v0.15.3',
   prometheusVersion: 'v2.52.0',
   nodeExporterVersion: 'v1.8.0',
   goSdkVersion: 'v0.6.2',
