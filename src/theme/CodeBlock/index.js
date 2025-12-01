e6adf98dfc51e083f707fa200cfd533d52cc799f
scl <sunchang_long@163.com>
2025-12-01 17:02:19 +0800

    fix: copy

---
+++ src/theme/CodeBlock/index.js
@@ -0,0 +1,7 @@
+// This file intentionally left minimal.
+// We don't need to wrap CodeBlock anymore because whitespace-safe copying
+// is handled by the swizzled CopyButton in `src/theme/CodeBlock/CopyButton/index.tsx`.
+
+import CodeBlock from '@theme-original/CodeBlock';
+
+export default CodeBlock;
