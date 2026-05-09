151b3aa03085fe07441587a237b2a7790422af87
liyang <daviderli614@gmail.com>
2026-05-09 14:45:16 +0800

    chore: update greptimedb operator version to v0.5.6 (#2462)
    Signed-off-by: liyang <daviderli614@gmail.com>

--- variables/variables-nightly.ts
+++ variables/variables-nightly.ts
@@ -7,5 +7,5 @@ export const variables = {
   debugPodVersion: '20250606-04e3c7d',
   etcdChartVersion: "12.0.8",
   etcdImageVersion: "3.6.1-debian-12-r3",
-  greptimedbOperatorVersion: 'v0.5.5'
+  greptimedbOperatorVersion: 'v0.5.6'
 };
